3cabd615b1ff63fa4c540a02bdf7222fb0fa4876
Rushor <PBienati@web.de>
2020-11-16 07:46:45 +0100

    Action: Rename action to ACTION_SET_CREATURE_SCALE

--- SAI-Editor/Forms/MainForm.Designer.cs
+++ SAI-Editor/Forms/MainForm.Designer.cs
@@ -1619,7 +1619,7 @@
             "ACTION_SET_COUNTER_RESET_OPTION",
             "ACTION_GO_DELETE",
             "ACTION_SET_LEVEL",
-            "ACTION_SET_OBJECT_SCALE"});
+            "ACTION_SET_CREATURE_SCALE"});
             this.comboBoxActionType.Location = new System.Drawing.Point(59, 45);
             this.comboBoxActionType.Name = "comboBoxActionType";
             this.comboBoxActionType.Size = new System.Drawing.Size(235, 21);
