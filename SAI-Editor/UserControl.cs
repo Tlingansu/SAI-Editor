6b3f64f4e2d5d5464bd5e86106f1f0ba72a255c7
Rushor <PBienati@web.de>
2021-09-09 13:06:48 +0200

    Action: SMART_ACTION_SUMMON_CREATURE - Param3 should have no box visible

--- SAI-Editor/UserControl.cs
+++ SAI-Editor/UserControl.cs
@@ -670,13 +670,16 @@ namespace SAI_Editor
                 case SmartAction.SMART_ACTION_CAST: //! Spell entry & Cast flags
                 case SmartAction.SMART_ACTION_INVOKER_CAST: //! Spell entry & Cast flags
                 case SmartAction.SMART_ACTION_CALL_CASTEDCREATUREORGO: //! Creature entry & Spell entry
-                case SmartAction.SMART_ACTION_SUMMON_CREATURE: //! Creature entry & Summon type
                 case SmartAction.SMART_ACTION_SET_UNIT_FIELD_BYTES_1: //! Bytes1flags & Type
                 case SmartAction.SMART_ACTION_REMOVE_UNIT_FIELD_BYTES_1: //! Bytes1flags & Type
                 case SmartAction.SMART_ACTION_RANDOM_PHASE_RANGE: //! Event phase 1 & 2
                     buttonActionParamOneSearch.Visible = true;
                     buttonActionParamTwoSearch.Visible = true;
                     buttonActionParamThreeSearch.Visible = true;
+                    break;
+                case SmartAction.SMART_ACTION_SUMMON_CREATURE: //! Creature entry & Summon type
+                    buttonActionParamOneSearch.Visible = true;
+                    buttonActionParamTwoSearch.Visible = true;
                     break;
                 case SmartAction.SMART_ACTION_CROSS_CAST:
                     buttonActionParamOneSearch.Visible = true; //! Spell entry
